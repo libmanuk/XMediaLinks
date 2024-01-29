d04bc2633721153f312840d5310521a68abd92df
Eric C. Weig <eweig@email.uky.edu>
2024-01-29 10:57:56 -0500

    Update XMediaLinksPlugin.php to add Library Location field

--- XMediaLinksPlugin.php
+++ XMediaLinksPlugin.php
@@ -15,6 +15,7 @@ class XMediaLinksPlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_filters = array(
         'xmedialink' => array('Display', 'Item', 'Item Type Metadata', 'External Digital Object Link'),
+        'xmedialink2' => array('Display', 'Item', 'Item Type Metadata', 'Library Location(s)'),
     );
 
 
@@ -22,9 +23,19 @@ class XMediaLinksPlugin extends Omeka_Plugin_AbstractPlugin
         return $this->_xmediaField($text, $args);
     }
 
-    public function _xmediaField($text, $args) {
-        $pieces = explode("^", $text);
-        return "<a href=\"" . $pieces[2] . "\" target=\"_blank\"><img src="" . $pieces[0] . "\" /></a><br/><p>. $pieces[1] . </p>";
+    public function xmedialink2($text, $args) {
+        return $this->_xmediaField2($text, $args);
     }
+
+    public function _xmediaField($text, $args) {
+        $pieces = explode('^', $text);
+        return "<table><tr><td style=\"width:15%;\"><a href=\"" . $pieces[2] . "\" target=\"_blank\"><img style=\"width:65px;padding:10px;\" id=\"medialink\" src=\"/nkaa/plugins/XMediaLinks/" . $pieces[0] . ".jpg\" alt=\"Hyperlink for external related resource\"/></a></td><td><span id=\"mediatext\">" . $pieces[1] . "</span></td></tr></table>";
+    }
+
+    public function _xmediaField2($text, $args) {
+        $pieces = explode('^', $text);
+        return "<table><tr><td style=\"width:15%;\"><a href=\"" . $pieces[1] . "\" target=\"_blank\">" . $pieces[0] . "</a></td></tr></table>";
+    }
+
 
 }
